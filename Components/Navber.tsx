d94dc6530dccd9260d1042c612a2d19f3d675727
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-05-16 17:39:44 +0600

    issuse with responsive solved

--- Components/Navber.tsx
+++ Components/Navber.tsx
@@ -15,7 +15,7 @@ export default function Navber() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="flex justify-between items-center px-5 py-10 relative">
+    <div className="flex justify-between items-center md:px-10 py-10 relative">
       {/* Logo */}
       <div className="lg:w-[15%] md:w-[35%] ">
         <Image
@@ -45,7 +45,7 @@ export default function Navber() {
       </div>
 
       {/* Auth Buttons (Desktop) */}
-      <div className="w-[15%] hidden lg:flex justify-center items-center gap-4">
+      <div className="w-[15%] hidden lg:flex justify-end items-center gap-4">
         <SignedOut>
           <div className="relative after:bg-white after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
             <SignInButton />
@@ -56,36 +56,52 @@ export default function Navber() {
         </SignedOut>
 
         <SignedIn>
-          <div className="relative after:bg-white after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
-            <UserButton />
-          </div>
+          <UserButton />
         </SignedIn>
       </div>
 
-      {/* Toggle Button for Mobile */}
-      <div className="lg:hidden z-50">
+      {/* Toggle Button & UserButton for Mobile/Tablet */}
+      <div className="lg:hidden z-50 flex items-center gap-4">
+        {/* UserButton always visible on mobile/tablet */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+        {/* Hamburger toggle */}
         <button onClick={() => setMenuOpen(!menuOpen)}>
           {menuOpen ? <X size={30} /> : <Menu size={30} />}
         </button>
       </div>
 
-      {/* Mobile + tablet Menu */}
+      {/* Mobile + Tablet Menu */}
       {menuOpen && (
         <div className="fixed top-0 left-0 w-full h-screen bg-black text-white flex flex-col justify-center items-center gap-10 z-40 text-2xl">
-          <li onClick={() => setMenuOpen(false)} className="cursor-pointer list-none">
+          <li
+            onClick={() => setMenuOpen(false)}
+            className="cursor-pointer list-none"
+          >
             <Link href="/courses">Courses</Link>
           </li>
-          <li onClick={() => setMenuOpen(false)} className="cursor-pointer list-none">
+          <li
+            onClick={() => setMenuOpen(false)}
+            className="cursor-pointer list-none"
+          >
             <Link href="/live-classes">Live Classes</Link>
           </li>
-          <li onClick={() => setMenuOpen(false)} className="cursor-pointer list-none">
+          <li
+            onClick={() => setMenuOpen(false)}
+            className="cursor-pointer list-none"
+          >
             <Link href="/ai-chat">Ai Chat</Link>
           </li>
-          <li onClick={() => setMenuOpen(false)} className="cursor-pointer list-none">
+          <li
+            onClick={() => setMenuOpen(false)}
+            className="cursor-pointer list-none"
+          >
             <Link href="/support">Support</Link>
           </li>
 
-          {/* Auth buttons in mobile */}
+          {/* Auth buttons in mobile menu */}
           <SignedOut>
             <div className="cursor-pointer" onClick={() => setMenuOpen(false)}>
               <SignInButton />
@@ -95,11 +111,8 @@ export default function Navber() {
             </div>
           </SignedOut>
 
-          <SignedIn>
-            <div className="cursor-pointer" onClick={() => setMenuOpen(false)}>
-              <UserButton />
-            </div>
-          </SignedIn>
+          {/* Hide UserButton here because it's outside toggle menu now */}
+          {/* If you want to keep it here as well, you can, but usually it's better outside */}
         </div>
       )}
     </div>
